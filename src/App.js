77076af77221715b860f8a8ae3d0e4681f75cded
coolistkovbel <coolistkovbel@gmail.com>
2022-09-06 12:00:36 -0400

    reading values from smart contract and put them into UI working with create trandsfer next

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { ethers } from "ethers";
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import abi from "./utils/MswPortal.json";
 
 function App() {
-  const [currenctAccount, setCurrentAccount] = useState("");
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [approverS, setApproverS] = useState("");
+  const [quoruM, setQuoruM] = useState("");
+
+  const contractAddress = "0x6e9F7074b894D2AD7B8bb73B18250280BefC7cC5";
+  const contractAbi = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -25,9 +32,44 @@ function App() {
       } else {
         console.log("No account found");
       }
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const mswPortalContract = new ethers.Contract(
+        contractAddress,
+        contractAbi,
+        signer
+      );
+
+      const approvers = await mswPortalContract.getApprovers();
+      const quorum = await mswPortalContract.quorum();
+
+      setApproverS(approvers);
+      setQuoruM(quorum);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const createTransfer = async (amount, to) => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const mswPortalContract = new ethers.Contract(
+          contractAddress,
+          contractAbi,
+          signer
+        );
+
+        let trans = await mswPortalContract.createTransfer(amount, to);
+        console.log(trans);
+      } else {
+        console.log("obj doesnt exist");
+      }
+    } catch (error) {}
   };
 
   useEffect(() => {
@@ -36,7 +78,24 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello {currenctAccount}</h1>
+      <header>
+        <ul>
+          <li>Approvers: {approverS.join(", ")}</li>
+          <li>Quorum: {quoruM.toString()}</li>
+        </ul>
+      </header>
+
+      <h1>Hello {currentAccount}</h1>
+
+      <div>
+        <form>
+          <label htmlFor="amount">Amount: </label>
+          <input id="amount" type="text" />
+          <label htmlFor="to">To: </label>
+          <input id="to" type="text" />
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
