b0d93c3a5835d26e5a897e1df427f3e8a3337728
coolistkovbel <coolistkovbel@gmail.com>
2022-09-06 02:26:12 -0400

    added check to see if there is ethereum object

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const checkIfWalletIsConnected = () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("makeSure you have metamask");
+    } else {
+      console.log("we have ethereum object: ", ethereum);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello world</h1>
